1acab52fc45a96fffa3dcbb7e84a1a5a20b390e9
codex1988 <filimonov.grigoriy.1988@gmail.com>
2025-01-08 09:50:47 +0500

    Implemented trim_line() and test_trim_line()

--- src/trim_line.c
+++ src/trim_line.c
@@ -1,3 +1,37 @@
 #include "trim_line.h"
 
-int trim_line(char s[], int lim) { return 0; }
+#include <stdio.h>
+
+int trim_line(char s[], int lim) {
+  int i = 0;             // read index
+  int y = 0;             // write index
+  int space_before = 0;  // zero - no space befor, one - there is space befor
+
+  // trim at the beginning
+  while (s[i] == '\n' || s[i] == '\t' || s[i] == ' ') {
+    ++i;
+  }
+
+  // trim in the middle and at the end
+  while (s[i] != '\0' && i < lim - 1) {
+    if (s[i] == '\n') {
+      s[y++] = '\n';
+      break;
+    }
+    if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t') {
+      if (space_before) {
+        i++;
+        continue;
+      }
+      s[y++] = ' ';
+      space_before = 1;
+    } else {
+      s[y++] = s[i++];
+      space_before = 0;
+    }
+  }
+
+  s[y] = '\0';
+
+  return 0;
+}
--- test/test_trim_line.c
+++ test/test_trim_line.c
@@ -2,7 +2,14 @@
 
 #include "trim_line.h"
 
-void test_trim_line() {}
+void test_trim_line() {
+  char s[] = "\t\t    hello    world    ";
+  trim_line(s, 100);
+  // int i;
+  // for(i = 0; i < 12; ++i) {
+  //   printf("%c\n", s[i]);
+  // } 
+}
 
 int main() {
   test_trim_line();
